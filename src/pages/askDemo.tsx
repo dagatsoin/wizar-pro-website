f0786977eaef8decc725198818c7b3e3fae3f8e6
dagatsoin <danil_fransman@hotmail.com>
2018-11-26 17:41:48 +0100

    send form

--- src/pages/askDemo.tsx
+++ src/pages/askDemo.tsx
@@ -4,15 +4,15 @@ import { Button, Grid, TextField } from '@material-ui/core'
 import { Layout } from '../../src/components'
 import style from './_askDemo.style'
 
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
+
 class AskDemoForm extends React.Component {
   public state = {
     email: '',
-  }
-
-  public handleChange = email => event => {
-    this.setState({
-      [email]: event.target.value,
-    })
   }
 
   public render() {
@@ -20,8 +20,9 @@ class AskDemoForm extends React.Component {
       <Layout>
         <Grid container justify="center">
           <form
-            style={style.root}
             noValidate
+            onSubmit={this.handleSubmit}
+            style={style.root}
             autoComplete="off"
             name="contact"
             method="post"
@@ -30,11 +31,12 @@ class AskDemoForm extends React.Component {
           >
             <TextField
               id="email"
+              name="email"
               label="Email"
               type="email"
               style={style.textField}
               value={this.state.email}
-              onChange={this.handleChange('email')}
+              onChange={this.handleChange}
               margin="normal"
             />
             <Button type="submit" color="primary" variant="contained">
@@ -44,6 +46,24 @@ class AskDemoForm extends React.Component {
         </Grid>
       </Layout>
     )
+  }
+
+  private handleChange = event => {
+    this.setState({
+      email: event.target.value,
+    })
+  }
+
+  private handleSubmit = e => {
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...this.state }),
+    })
+      .then(() => alert('Merci ! On vous rappelle très vite !'))
+      .catch(error => alert(error))
+
+    e.preventDefault()
   }
 }
 
